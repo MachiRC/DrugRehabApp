af7d9eace298d909d2a344e3509e596a7dd39d8c
MachiRC <macks1006@gmail.com>
2025-09-22 10:34:09 -0400

    Added Firebase
    Firebase data server thingy

--- DrugRehab/app/build.gradle.kts
+++ DrugRehab/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services") // Add the Google services Gradle plugin
 }
 
 android {
@@ -9,7 +10,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.drugrehab"
+        applicationId = "com.dmzco.rehabits"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -56,4 +57,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+
 }
